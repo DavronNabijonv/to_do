96c87dd1b3f8dc5eac9e5395b56cb261d615a730
Davron Chetin <nabijonovdavronbek619@gmail.com>
2025-04-26 11:00:45 +0500

    dark mode and middlewer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 // import { useState } from 'react'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import './App.css'
 import EntrancePage from './page_parts/entrancePage';
 import MainPage from './page_parts/mainPage';
 import { ToastContainer } from 'react-toastify';
+import { useEffect } from "react";
 
 
 function App() {
 
+  const navigate = useNavigate();
+  useEffect(()=>{
+    const token = localStorage.getItem('token');
+    if(token){
+      navigate('/main')
+    }else{
+      navigate('/')
+    }
+  },[])
 
   return (
     <>
